799b0d0a997465ae5a1704b5f23211f0e436975a
m4Pro-amITRanquil <tranquil.95@gmail.com>
2025-11-20 14:29:44 +0300

    fix: Windows penceresi baştan ekran boyutunda oluşturuluyor
    - GetSystemMetrics ile ekran boyutu alınıyor
    - Pencere 1280x720 yerine doğrudan tam ekran boyutunda oluşturuluyor
    - Artık boyut değişimi titremesi olmayacak

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,8 +27,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+
+  // Get screen dimensions for maximized window
+  int screen_width = ::GetSystemMetrics(SM_CXSCREEN);
+  int screen_height = ::GetSystemMetrics(SM_CYSCREEN);
+
+  Win32Window::Point origin(0, 0);
+  Win32Window::Size size(screen_width, screen_height);
   if (!window.Create(L"Konsinye", origin, size))
   {
     return EXIT_FAILURE;
