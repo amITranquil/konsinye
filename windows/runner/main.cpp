a2f30b217759fc8f2c1de9c034e21803087dbde7
m4Pro-amITRanquil <tranquil.95@gmail.com>
2025-11-20 15:05:27 +0300

    fix: Windows main.cpp origin ve size parametreleri comment out edildi
    - Win32Window::Point origin ve Size parametreleri yoruma alındı
    - window.Create() çağrısı sadece SW_MAXIMIZE kullanıyor

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,8 +27,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(0, 0);
-  Win32Window::Size size(800, 600);
+  //Win32Window::Point origin(0, 0);
+  //Win32Window::Size size(800, 600);
   if (!window.Create(L"Konsinye", origin, size))
   {
     return EXIT_FAILURE;
