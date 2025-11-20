9961b1e8ea9e29050ec6715e19dfb4363bc815d0
m4Pro-amITRanquil <tranquil.95@gmail.com>
2025-11-20 15:17:17 +0300

    refactor: Windows main.cpp temizlendi
    - Gereksiz comment'ler kaldırıldı
    - origin ve size değişkenleri geri eklendi (varsayılan başlangıç boyutu)
    - ShowWindow çağrısı kaldırıldı (flutter_window.cpp'de SetNextFrameCallback içinde hallediliyor)
    - Code formatting düzeltmeleri

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -15,28 +15,25 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
     CreateAndAttachConsole();
   }
 
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
-
   std::vector<std::string> command_line_arguments =
       GetCommandLineArguments();
-
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  //Win32Window::Point origin(0, 0);
-  //Win32Window::Size size(800, 600);
+
+  // Varsayılan başlangıç boyutu (önemsiz çünkü maximize edeceğiz)
+  Win32Window::Point origin(0, 0);
+  Win32Window::Size size(800, 600);
+
   if (!window.Create(L"Konsinye", origin, size))
   {
     return EXIT_FAILURE;
   }
-  window.SetQuitOnClose(true);
 
-  // Maximize the window on startup
-  ::ShowWindow(window.GetHandle(), SW_MAXIMIZE);
+  window.SetQuitOnClose(true);
 
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0))
